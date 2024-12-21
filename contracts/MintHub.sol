e2074dc3b86bcf3325b4fd5e80702561991cc553
LikemDzokoto <likemdzokoto@gmail.com>
2024-12-21 19:55:45 +0000

    setup base & setup contract

--- contracts/MintHub.sol
+++ contracts/MintHub.sol
@@ -1,7 +1,71 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.28;
 
 
-contract MintHub {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+
+contract MintHub is ERC721URIStorage {
+     using Counters for Counters.Counter;
+     Counters.Counter private _nftId;
+     Counters.Counter private _soldItems;
+
+
+
+    address payable private owner;
+
+
+     
+
+
+    struct mintItem{
+        uint256 _nftid;
+        uint256 tokenId;
+        address nftContract;
+        address payable creator;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+
+        }
+
+     /* =========== MAPPINGS ============ */
+
+     mapping(uint256 => mintItem) private idToMintHubItem ;
+
+
+      /* =========== EVENTS ============ */
+      event MarketplaceItemCreated(
+        uint256 indexed _nftId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address creator,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+
+     /* =========== CONSTRUCTOR ============ */
+
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+
+
+
+
+
+
+
+
+
     
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,56 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+  solidity: {
+    compilers:[
+      {
+      version: "0.8.28",
+      settings:{
+        evmVersion:"paris",
+        optimizer: {
+          enabled:true,
+          runs:200
+      }
+      }
+    }
+    ],
+  },
+
+  networks:{
+    hardhat: {
+      allowUnlimitedContractSize:true,
+  },
+
+
+  //base-sepolia 
+  baseSepolia: {
+    url: "https://sepolia.base.org",
+    chainId: 84532,
+    accounts: [process.env.PRIVATE_KEY!],
+  },
+},
+
+etherscan: {
+  apiKey: {
+    baseSepolia: "getanAPIKEYLOL"
+  },
+  customChains:[
+    {
+    
+      network: "baseSepolia",
+      chainId: 84532,
+      urls: {
+        apiURL: "https://base-sepolia.blockscout.com/api",
+        browserURL: "https://base-sepolia.blockscout.com",
+      },
+    }
+
+  ]
+
+
+}
+}
+
+
 
 export default config;
